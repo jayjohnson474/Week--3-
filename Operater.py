4994f82d9e9a7256a022f8e78ece57beea7c4b77
jayjohnson474 <jjohnson3@stu.socsd.org>
2024-09-25 10:08:32 -0400

    Update Operater.py
    --

---
+++ Operater.py
@@ -0,0 +1,26 @@
+#Jayanth Johnson
+#September 24
+#Operater Practice
+
+print((5*(2-3)))
+print("2 + 2 = ",2+2, "as you can see")
+print(2+2)
+
+print (9/5)
+print (9.0/5)
+print (9/5.0)
+print (9.0/5.0)
+
+print (9//5)
+print (9.0//5)
+print (9//5.0)
+print (9.0//5.0)
+
+print(27//5)
+print(27%5)
+print(9//4)
+print(9%4)
+print(21//2)
+print(21%2)
+print(5//8)
+print(5%8)
